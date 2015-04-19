55292fa2ec06bb3d0e9c72ab65ad25eedecff670
Dan Bradham <danielbradham@gmail.com>
2015-04-19 09:58:27 -0400

    Fuly implemented documented api

--- psforms/__init__.py
+++ psforms/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'psforms'
 __author__ = 'Dan Bradham'
 __email__ = 'danielbradham@gmail.com'
 __url__ = 'http://github.com/danbradham/apptemplate.git'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __license__ = 'MIT'
 __description__ = '''psforms'''
 
